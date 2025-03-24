c246bbdc11bfc98e7c4aeee07d3afee768ee73e0
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-25 00:13:47 +0900

    add server action

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { ActionFunctionArgs, MetaFunction } from "@remix-run/node";
+import { Form, useActionData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,5 +9,34 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  return <div>content</div>;
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <div>
+      <h1 className="text-4xl font-extrabold py-2">Server Action</h1>
+      <Form method="post" className="flex flex-col items-start gap-y-2 my-5">
+        <label>
+          Name:
+          <input
+            name="name"
+            className="ml-2 bg-gray-800 px-2 py-1 rounded my-4"
+          />
+        </label>
+        <button
+          type="submit"
+          className="border border-gray-200 px-2 py-1 rounded my-4"
+        >
+          Get greeting
+        </button>
+      </Form>
+      <p>Message: {actionData?.message}</p>
+    </div>
+  );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const name = formData.get("name");
+
+  return { message: `Hello, ${name}!` };
 }
